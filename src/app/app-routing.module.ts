37cf8c4132ec7ddc228a13edcf65d9af4b6d1f18
ForzaNapoli <albertolandi1@gmail.com>
2019-05-17 16:03:58 -0400

    Rutas LogIn-SignUp

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+const routes: Routes = [
+  { path: '', component: LoginComponent  },
+  { path: 'signup', component: SignupComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

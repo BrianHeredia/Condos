b00189c327bb4ae21103856bafb53c1f3a13e9a4
BrianHeredia <brianheredia@gmail.com>
2019-05-17 13:03:10 -0430

    solucion de conflico de versiones

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,22 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
-<<<<<<< HEAD
-import { SingupComponent } from './components/singup/singup.component';
-import { SignupComponent } from './components/signup/signup.component';
-=======
 import { LoginComponent } from './components/login/login.component';
->>>>>>> 90cda2a0fc2e50b90543c81529c0004c6272e86d
+import { SignupComponent } from './components/signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-    SingupComponent,
+    LoginComponent,
     SignupComponent
-=======
-    LoginComponent
->>>>>>> 90cda2a0fc2e50b90543c81529c0004c6272e86d
   ],
   imports: [
     BrowserModule,

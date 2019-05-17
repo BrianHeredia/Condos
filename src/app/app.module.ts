1472ad9d1e46260eb6f1ea765b6c99b684ea016e
BrianHeredia <brianheredia@gmail.com>
2019-05-17 12:33:03 -0430

    Agregar signup component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { SingupComponent } from './components/singup/singup.component';
+import { SignupComponent } from './components/signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SingupComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
